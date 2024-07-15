254edca00800019ff6205d8b78e1aa571d1e21d0
Kuan <kuan.pang@mail.utoronto.ca>
2024-07-15 08:39:27 -0400

    multiple rep visualization

--- pages/8_Visualize_Result.py
+++ pages/8_Visualize_Result.py
@@ -27,40 +27,41 @@ if entry_id:
         data = response.json()
 
         # Extracting readings and hover text
-        wells = sorted(data['0'].keys())
-        rows = sorted(list(set(well[0] for well in wells)), reverse=True)
-        columns = sorted(list(set(int(well[1:]) for well in wells)))
+        for key in data.keys():
+            wells = sorted(data[key].keys())
+            rows = sorted(list(set(well[0] for well in wells)), reverse=True)
+            columns = sorted(list(set(int(well[1:]) for well in wells)))
 
-        heatmap_data = np.zeros((len(rows), len(columns)))
-        hovertext = np.empty((len(rows), len(columns)), dtype=object)
+            heatmap_data = np.zeros((len(rows), len(columns)))
+            hovertext = np.empty((len(rows), len(columns)), dtype=object)
 
-        for well in wells:
-            row_idx = rows.index(well[0])
-            col_idx = columns.index(int(well[1:]))
-            heatmap_data[row_idx, col_idx] = data['0'][well]['reading']
+            for well in wells:
+                row_idx = rows.index(well[0])
+                col_idx = columns.index(int(well[1:]))
+                heatmap_data[row_idx, col_idx] = data[key][well]['reading']
 
-            components = data['0'][well]['components']
-            hovertext[row_idx, col_idx] = (f"Well: {well}<br>Reading: {data['0'][well]['reading']}"
-                                           f"<br>Amines: {components['amines']}<br>Isocyanide: {components['isocyanide']}"
-                                           f"<br>Lipid Aldehyde: {components['lipid_aldehyde']}"
-                                           f"<br>Lipid Carboxylic Acid: {components['lipid_carboxylic_acid']}"
-                                           )
+                components = data['0'][well]['components']
+                hovertext[row_idx, col_idx] = (f"Well: {well}<br>Reading: {data[key][well]['reading']}"
+                                            f"<br>Amines: {components['amines']}<br>Isocyanide: {components['isocyanide']}"
+                                            f"<br>Lipid Aldehyde: {components['lipid_aldehyde']}"
+                                            f"<br>Lipid Carboxylic Acid: {components['lipid_carboxylic_acid']}"
+                                            )
 
-        fig = go.Figure(data=go.Heatmap(
-            z=heatmap_data,
-            x=[str(col) for col in columns],
-            y=rows,
-            text=hovertext,
-            hoverinfo="text",
-            colorscale='Viridis'
-        ))
+            fig = go.Figure(data=go.Heatmap(
+                z=heatmap_data,
+                x=[str(col) for col in columns],
+                y=rows,
+                text=hovertext,
+                hoverinfo="text",
+                colorscale='Viridis'
+            ))
 
-        fig.update_layout(
-            title='96-well Plate Readings Heatmap',
-            xaxis_title='Column',
-            yaxis_title='Row'
-        )
+            fig.update_layout(
+                title=f'96-well Plate Readings Heatmap; rep {key}',
+                xaxis_title='Column',
+                yaxis_title='Row'
+            )
 
-        st.plotly_chart(fig)
+            st.plotly_chart(fig)
     else:
         st.error('Failed to fetch data. Please check the entry ID.')
